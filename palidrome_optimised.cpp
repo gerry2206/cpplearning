f8ea81df69d115433a7b3095cbc7ecf062728094
Gerry <gerry2206>
2022-08-30 01:01:26 +0530

    palindrome optimised

---
+++ palidrome_optimised.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+int hsh[N][26];
+
+int main(){
+	for(int i=0; i< N; ++i){
+		for(int j=0; j<26;++j){
+			hsh[i][j] = 0;
+		}
+	}
+	int t;
+	cin >> t;
+	while(t--){
+		int n, q;
+		cin >> n >> q;
+		string s;
+		cin >> s;
+		for(int i=0; i<n; ++i){
+			hsh[i+1][s[i] - 'a']++;
+		}
+		for(int i=0; i<26; ++i){
+			for(int j=1; j<=n; ++j){
+				hsh[j][i] += hsh[j-1][i];
+			}
+		}
+		while(q--){
+			int l, r;
+			cin >> l >> r;
+			int oddCt = 0;
+			for(int i=0 ; i<26; ++i){
+				int charCt= hsh[r][i] - hsh[l-1][i];
+				if(charCt % 2 != 0) oddCt++;
+			}
+			if(oddCt >1) cout << "NO\n";
+			else cout << "YES\n";
+		}
+	}
+}
+				
+			
+	
